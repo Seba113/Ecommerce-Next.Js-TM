2735aab1ef04cd7c610136ac992c978618c3126b
Sebastian Fernandez <sebafernandez420@gmail.com>
2024-12-26 20:21:39 -0300

    fix: update product fetching to use correct data structure; enhance ChooseCategory component to display categories with links and images

--- api/getProducts.tsx
+++ api/getProducts.tsx
@@ -11,7 +11,7 @@ export function useGetCategories() {
             try{
                 const res = await fetch(url);
                 const json = await res.json();
-                setResult(json);
+                setResult(json.data);
                 setLoading(false);
             }catch{
                 setError(error);
--- components/ui/choose-category.tsx
+++ components/ui/choose-category.tsx
@@ -2,13 +2,37 @@
 import { useGetCategories } from '@/api/getProducts'
 import Link from 'next/link'
 import { ResponseType } from '@/types/response'
+import { CategoryType } from '@/types/category';
 
 
 
 export function ChooseCategory() {
     const {result, loading, error}: ResponseType = useGetCategories();
-    console.log(result)
     return (
-        <p>{}</p>
-    )
+        <div className='max-w-6xl py-4 mx-auto sm:py-16 sm:px-24'>
+            <h3 className='px-6 pb-4 text-3xl sm:pb-8'>Elegí tu categoría favorita</h3>
+            <div className='grid gap-5 md:grid-cols-2 lg:grid-cols-3'>
+                {!loading && result !== undefined && (
+                    result.map((category: CategoryType) => (
+                        <Link 
+                            key={category.id} 
+                            href={`/category/${category.slug}`}
+                            className='relative max-w-xs mx-auto overflow-hidden bg-no-repeat bg-cover rounded'
+                        >
+                            <img 
+                                src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${category.image.url}`} 
+                                alt={category.categoryName} 
+                                className='max-w-[270px] transition duration-300 ease-in-out rounded-lg hover:scale-110'
+                            />
+                            <p className='absolut w-full font-bold text-center py-2 text-lg text-white bottom-5 backdrop-blur-lg'>
+                                {category.categoryName}
+                            </p>
+                        </Link>    
+                    )
+
+                    )
+                )}
+            </div>
+        </div>
+    ) 
 }
