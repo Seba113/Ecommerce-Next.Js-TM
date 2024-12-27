52c2a0c0e0a508cca1c77cc81267d17feb4095f1
Sebastian Fernandez <sebafernandez420@gmail.com>
2024-12-27 12:19:04 -0300

    add: create ProductCard component for category page; implement loading skeleton and product display

--- app/(routes)/category/[categorySlug]/page.tsx
+++ app/(routes)/category/[categorySlug]/page.tsx
@@ -3,6 +3,10 @@ import { useGetCategoryProduct } from "@/api/getCategoryProduct"
 import { useParams, useRouter } from "next/navigation"
 import { ResponseType } from "@/types/response"
 import { Separator } from "@/components/ui/separator"
+import SkeletonSchema from "@/components/ui/skeletonSchema"
+import ProductCard from "./product-card"
+import { ProductType } from "@/types/product"
+
 
 
 export default function Page() {
@@ -10,14 +14,22 @@ export default function Page() {
     const {categorySlug} = params
     const {result, loading, error}: ResponseType = useGetCategoryProduct(categorySlug)
     const router = useRouter()
-    console.log(result)
     return (
         <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
             {result !== null && !loading &&(
                 <h1 className="text-3xl font-medium">{result[0].category.categoryName}</h1>
             )}
             <Separator className="m-1"/>
-            <div>
+            <div className="grid gap-5 mt-8 sm:grid-cols-2 md:grid-cols-3 md:gap-10">
+                {loading && (
+                    <SkeletonSchema grid={3} />
+                )}
+                {result !== null && !loading && (
+                    result.map((product : ProductType) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))
+                    
+                )}
                 
             </div>
 
---
+++ app/(routes)/category/[categorySlug]/product-card.tsx
@@ -0,0 +1,41 @@
+import { Carousel, CarouselContent, CarouselItem } from "@/components/ui/carousel"
+import { IconButton } from "@/components/ui/icon-button"
+import { ProductType } from "@/types/product"
+import { Expand, ShoppingCart } from "lucide-react"
+import Link from "next/link"
+import { useRouter } from "next/navigation"
+
+type ProductTypeProps = {
+    product: ProductType
+}
+
+const ProductCard = (props: ProductTypeProps) => {
+    const {product} = props;
+    const router = useRouter()
+    return(
+        <Link href={`/product/${product.slug}`} className="relative p-2 transition-all duration-100 rounded-lg hover:shadow-md">
+            <Carousel 
+                opts={{align: "start"}}
+                className="w-full max-w-sm"
+            >
+                <CarouselContent>
+                    {product.images.map((image) => (
+                        <CarouselItem key={image.id} className="group">
+                            <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${image.url}`} alt={image.name} className="rounded-xl"/>
+                            <div className="absolute w-full transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
+                                <div className="flex justify-center gap-x-6">
+                                    <IconButton onClick={() => router.push(`/product/${product.slug}`) } icon={<Expand size={20} className="text-gray-600 " />} />
+                                    <IconButton onClick={() => console.log("product")} icon={<ShoppingCart size={20} className="text-gray-600" />} />
+                                </div>
+                            </div>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+            </Carousel>
+            <p className="text-1xl text-center">{product.productName}</p>
+            <p className="font-bold text-center">$ {product.price}</p>
+        </Link>
+    )
+}
+
+export default ProductCard
