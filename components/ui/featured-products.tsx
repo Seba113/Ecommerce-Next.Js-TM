4db5024e2ab083f2f49d40b2821227d32a2831fd
Sebastian Fernandez <sebafernandez420@gmail.com>
2025-01-25 18:28:42 -0300

    refactor: clean up FeaturedProducts component code and improve layout structure

--- components/ui/featured-products.tsx
+++ components/ui/featured-products.tsx
@@ -12,11 +12,11 @@ import { useCart } from "@/hooks/use-cart"
 
 export const FeaturedProducts = () => {
 
-    const {loading, result}: ResponseType = useGetFeaturedProducts();
+    const { loading, result }: ResponseType = useGetFeaturedProducts();
     const router = useRouter();
-    const {addItem, items} = useCart();
-    
-    
+    const { addItem, items } = useCart();
+
+
     return (
 
         <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
@@ -27,44 +27,48 @@ export const FeaturedProducts = () => {
                         <SkeletonSchema grid={3} />
                     )}
                     {result !== null && (
-                        result.map((product: ProductType) =>{
-                            const {images, id, slug, productName, category} = product;
-                            const {categoryName} = category;
-                            
-                            
+                        result.map((product: ProductType) => {
+                            const { images, id, slug, productName, category } = product;
+                            const { categoryName } = category;
+
+
                             return (
                                 <CarouselItem key={id} className="md:basis-1/2 lg:basis-1/3 group">
                                     <div className="p-1">
                                         <Card className="py-4 border-gray-200 shadow-none">
                                             <CardContent className="relative flex items-center justify-center px-6 py-2">
-                                                <img 
-                                                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images[0].url}`} 
+                                                <img
+                                                    src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images[0].url}`}
                                                     alt="Image Featured" />
-                                                    <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
-                                                        <div className="flex justify-center gap-x-6">
-                                                            <IconButton 
-                                                            onClick={()=> router.push(`product/${slug}`)} 
-                                                            icon={<Expand size={20}/>} 
-                                                            className="text-gray-600"
-                                                            />
+                                                <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
 
-                                                            <IconButton 
-                                                            onClick={()=> addItem(product)} 
-                                                            icon={<ShoppingCart size={20}/>} 
-                                                            className="text-gray-600"/>
-                                                            
-                                                        </div>
-                                                    </div>
-                                                    
+                                                </div>
+
                                             </CardContent>
 
-                                            <div className="flex  justify-center gap-4 px-8">
+                                            <div className="flex justify-center gap-4 px-8">
                                                 <h3 className="text-lg font-bold">{productName}</h3>
-                                                
+                                            </div>
+                                            <div className="flex justify-center gap-4 px-8">
+                                                <p>$ {product.price}</p>
+                                            </div>
+                                            <div className="flex justify-center gap-x-6">
+                                                <IconButton
+                                                    onClick={() => router.push(`product/${slug}`)}
+                                                    icon={<Expand size={20} />}
+                                                    className="text-gray-600"
+                                                />
+
+                                                <IconButton
+                                                    onClick={() => addItem(product)}
+                                                    icon={<ShoppingCart size={20} />}
+                                                    className="text-gray-600"
+                                                 />
+
                                             </div>
                                         </Card>
                                     </div>
-                                
+
                                 </CarouselItem>
                             )
                         })
