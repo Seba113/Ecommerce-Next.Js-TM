bc3153234b47fc72f5461e614121e7caee0e99c8
Sebastian Fernandez <sebafernandez420@gmail.com>
2025-01-25 22:21:51 -0300

    refactor: replace img with Image component for better optimization in ProductCard

--- app/(routes)/category/[categorySlug]/product-card.tsx
+++ app/(routes)/category/[categorySlug]/product-card.tsx
@@ -5,6 +5,7 @@ import { ProductType } from "@/types/product"
 import { Expand, ShoppingCart } from "lucide-react"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
+import Image from "next/image"
 
 type ProductTypeProps = {
     product: ProductType
@@ -22,7 +23,7 @@ const ProductCard = (props: ProductTypeProps) => {
                 <CarouselContent>
                     {product.images.map((image) => (
                         <CarouselItem key={image.id} className="group">
-                            <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${image.url}`} alt={image.name} className="rounded-xl mx-auto"/>
+                            <Image src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${image.url}`} alt={image.name} className="rounded-xl mx-auto"/>
                             <div className="absolute w-full transition duration-200 opacity-0 group-hover:opacity-100 bottom-5">
                                 <div className="flex justify-center gap-x-6">
                                     <IconButton onClick={() => router.push(`/product/${product.slug}`) } icon={<Expand size={20} className="text-gray-600 " />} />
